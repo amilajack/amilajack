088850ebfbaec20e72325add7e8a258a24bdb64a
Amila Welihinda <amilajack@gmail.com>
2019-11-24 19:45:10 -0800

    add linting support

--- amilajack.js
+++ amilajack.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-const { version } = require('./package.json');
 const figlet = require('figlet');
 const chalk = require('chalk');
 const Table = require('cli-table3');
@@ -9,15 +8,17 @@ const pkg = require('./package.json');
 const fonts = figlet.fontsSync();
 const randomFont = fonts[Math.round(Math.random() * fonts.length)];
 
-console.log(figlet.textSync('amilajack', {
-  font: randomFont
-}));
+console.log(
+  figlet.textSync('amilajack', {
+    font: randomFont
+  })
+);
 
 const table = new Table({
   head: [chalk.bold('Website'), chalk.bold('Account')]
 });
 
-console.log(chalk.bold('my personal w̶e̶b̶s̶i̶t̶e̶ npm module'))
+console.log(chalk.bold('my personal w̶e̶b̶s̶i̶t̶e̶ npm module'));
 
 const accounts = [
   ['github', 'https://github.com/amilajack'],
@@ -27,10 +28,10 @@ const accounts = [
   ['linkedin', 'https://www.linkedin.com/in/amilajack/'],
   ['resume', 'https://github.com/amilajack/resume'],
   ['paypal donations', 'https://paypal.me/amilajack'],
-  ['venmo donations', 'https://venmo.com/amilajack'],
+  ['venmo donations', 'https://venmo.com/amilajack']
 ];
 
 accounts.forEach(account => table.push(account));
-console.log(table.toString())
+console.log(table.toString());
 
 updateNotifier({ pkg }).notify();
--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const childProcess = require('child_process');
 const { default: Joker } = require('@amilajack/joker');
 
 const binPath = require.resolve('../amilajack');
 
 describe('e2e', () => {
-  it('should render correctly', (done) => {
-    new Joker({ color: false, newLine: false })
-      .run(`node ${binPath}`)
-      .expect(({ stdout })=> {
-        expect(stdout).toContain('email');
-        expect(stdout).toContain('github');
-      })
-      .end(done);
+  it('should render correctly', () => {
+    return new Promise(resolve => {
+      return new Joker({ color: false, newLine: false })
+        .run(`node ${binPath}`)
+        .expect(({ stdout }) => {
+          expect(stdout).toContain('email');
+          expect(stdout).toContain('github');
+        })
+        .end(resolve);
+    });
   });
 });
